d68beb406fcad2d727100c6d51a76a2af6a118dd
Daniel Miles Douglas <dougl215@sdfiana007.sdf.slac.stanford.edu>
2025-08-05 14:30:06 -0700

    some edits to submission script

--- examples/submit_batchsim_job.sh
+++ examples/submit_batchsim_job.sh
@@ -15,21 +15,31 @@
 
 SINGULARITY_IMAGE_PATH=/sdf/group/neutrino/images/larcv2_ub22.04-cuda12.1-pytorch2.2.1-larndsim.sif
 
+# INPUT_EDEPSIM=$1
+# GAMPIXPY_OUTPUT=$2
+# ROTATED_OUTPUT=$3
+
 INPUT_EDEPSIM=$1
-GAMPIXPY_OUTPUT=$2
-ROTATED_OUTPUT=$3
+ROTATED_OUTPUT=$2
+
+ID=$(cat /proc/sys/kernel/random/uuid) 
+TMP_GAMPIXPY_OUTPUT=/lscratch/dougl215/tmp/${ID}.h5
 
 # READOUT_CONFIG=$3
 # DETECTOR_CONFIG=$4
 
 GAMPIXROOT=${HOME}/studies/GAMpix/GAMPixPy
 
-COMMAND="python3 ${GAMPIXROOT}/examples/batch_sim.py ${INPUT_EDEPSIM} -o ${GAMPIXPY_OUTPUT} -r ${GAMPIXROOT}/gampixpy/readout_config/GAMPixD.yaml -d ${GAMPIXROOT}/gampixpy/detector_config/coh_250.yaml"
+COMMAND="python3 ${GAMPIXROOT}/examples/batch_sim.py ${INPUT_EDEPSIM} -o ${TMP_GAMPIXPY_OUTPUT} -r ${GAMPIXROOT}/gampixpy/readout_config/GAMPixD.yaml -d ${GAMPIXROOT}/gampixpy/detector_config/coh_250.yaml"
 # COMMAND="python3 ${GAMPIXROOT}/examples/batch_sim.py ${INPUT_EDEPSIM} -o ${TMP_FILE} -r ${READOUT_CONFIG} -d ${DETECTOR_CONFIG}"
 
+echo $COMMAND
 singularity exec --nv -B /sdf,/lscratch ${SINGULARITY_IMAGE_PATH} ${COMMAND}
 
-COMMAND="python3 ${GAMPIXROOT}/examples/rotate_to_experimental_coordinates.py ${GAMPIXPY_OUTPUT} -o ${ROTATED_OUTPUT} -d ${GAMPIXROOT}/gampixpy/detector_config/coh_250.yaml"
+COMMAND="python3 ${GAMPIXROOT}/examples/rotate_to_experimental_coordinates.py ${TMP_GAMPIXPY_OUTPUT} -o ${ROTATED_OUTPUT} -d ${GAMPIXROOT}/gampixpy/detector_config/coh_250.yaml"
 # COMMAND="python3 ${GAMPIXROOT}/examples/rotate_to_experimental_coordinates.py ${TMP_FILE} -o ${OUTPUT_HDF5} -d ${DETECTOR_CONFIG}"
 
+echo $COMMAND
 singularity exec --nv -B /sdf,/lscratch ${SINGULARITY_IMAGE_PATH} ${COMMAND}
+
+rm $TMP_GAMPIXPY_OUTPUT
