8ee27a13d07301006dd86a8d1e0f84398bef257b
Daniel Douglas <dougl215@slac.stanford.edu>
2025-10-29 16:55:17 -0700

    get t0_start/end instead of t_start/end

--- gampixpy/input_parsing.py
+++ gampixpy/input_parsing.py
@@ -585,8 +585,8 @@ class EdepSimParser (SegmentParser):
                             event_segments['z_end']*cm]).T
         end_pos = torch.tensor(end_pos)
         
-        start_time = torch.tensor(event_segments['t_start']*ns)
-        end_time = torch.tensor(event_segments['t_end']*ns)
+        start_time = torch.tensor(event_segments['t0_start']*ns)
+        end_time = torch.tensor(event_segments['t0_end']*ns)
         
         dE = torch.tensor(event_segments['dE']*MeV)
         pdgid = torch.tensor(event_segments['pdg_id'])
@@ -627,13 +627,13 @@ class EdepSimParser (SegmentParser):
         start_4vec = np.array([event_segments['x_start']*cm,
                                event_segments['y_start']*cm,
                                event_segments['z_start']*cm,
-                               event_segments['t_start']*ns,
+                               event_segments['t0_start']*ns,
                                ]).T
         start_4vec = torch.tensor(start_4vec)
         end_4vec = np.array([event_segments['x_end']*cm,
                              event_segments['y_end']*cm,
                              event_segments['z_end']*cm,
-                             event_segments['t_end']*ns,
+                             event_segments['t0_end']*ns,
                              ]).T
         end_4vec = torch.tensor(end_4vec)
         dE = torch.tensor(event_segments['dE']*MeV)
